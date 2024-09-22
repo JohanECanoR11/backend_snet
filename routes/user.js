e688614109baaddba0e9bc01db2dbbe30f40f79c
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-22 00:18:54 -0500

    method Login with JWT ok

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 const router = Router();
-import { register } from "../controllers/user.js";
+import { login, register, testUser } from "../controllers/user.js";
 
 // Definir las rutas
+router.get('/test-user', testUser);
 router.post('/register', register);
+router.post('/login', login);
 
 // Exportar el Router
 export default router;
