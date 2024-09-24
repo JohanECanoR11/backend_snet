b7e0c2ef14d26952b9ce81bb6cb409c2e08a73ec
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-23 20:37:59 -0500

    create method user profile

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 const router = Router();
-import { login, register, testUser } from "../controllers/user.js";
+import { login, profile, register, testUser } from "../controllers/user.js";
 import { ensureAuth } from "../middlewares/auth.js";
 
 // Definir las rutas
 router.get('/test-user', ensureAuth, testUser);
 router.post('/register', register);
 router.post('/login', login);
+router.get('/profile/:id', ensureAuth, profile);
 
 // Exportar el Router
 export default router;
